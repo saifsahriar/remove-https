39d6c5ef0607c386712548dd60d233cc11b2b039
Saif Sahriar <119245305+saifsahriar@users.noreply.github.com>
2024-05-22 17:24:19 +0530

    Create remove.py

---
+++ remove.py
@@ -0,0 +1,24 @@
+#! /usr/bin/python3
+
+import sys
+import re
+
+if(len(sys.argv) < 2):
+    print("[-] Insufficient parameters")
+    print("[+] Usage: python3 <filename>")
+
+filename = sys.argv[1]
+output_file = "output.txt"
+
+try:
+    with open(filename, 'r') as file, open(output_file, 'w') as op_file:
+        lines = file.readlines()
+        pattern = re.compile(r'https?://')
+        for line in lines:
+            line = pattern.sub('', line)
+            op_file.write(line)
+
+except FileNotFoundError:
+    print(f"{filename} not found")
+    sys.exit(1)
+
